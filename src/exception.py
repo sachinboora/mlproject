899dcbfdd0fbf000fc9bb20dc57fb50f322f2814
Sachin Boora <99988814+sachinboora@users.noreply.github.com>
2024-03-13 17:31:18 +0530

    data and model training

--- src/exception.py
+++ src/exception.py
@@ -1,7 +1,7 @@
 ##custom exception handling
 
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
