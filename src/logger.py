0a897ec421ac27a3a22f9e6fe5153382b2c5cb50
Sachin Boora <99988814+sachinboora@users.noreply.github.com>
2024-03-15 14:36:27 +0530

    logs not creating issue resolved

--- src/logger.py
+++ src/logger.py
@@ -5,16 +5,16 @@ import logging
 import os
 from datetime import datetime
 
-LOGFILE = f"{datetime.now().strftime('%M_%d_%Y_%H:%M_%S')}.log"
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-logs_path = os.path.join(os.getcwd(), "Logs", LOGFILE)
+logs_path = os.path.join(os.getcwd(), "Logs", LOG_FILE)
 
 os.makedirs(logs_path, exist_ok=True)
 
-LOG_FILE_PATH = os.path.join(logs_path, LOGFILE)
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format='[%(asctime)s] %(lineno)d %(name)s -%(levelname)s - %(message)s',
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO
 )
